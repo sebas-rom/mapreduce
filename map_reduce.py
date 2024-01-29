f24144f8db1ea29920d49173de734d0928037b79
Kristian Mendoza <saidmendoza730@gmail.com>
2024-01-29 18:36:39 -0500

    changed to original

--- map_reduce.py
+++ map_reduce.py
@@ -3,28 +3,26 @@ import os
 import re
 import json
 
-def read_chunk(file_path, encoding='utf-8'):
-    with open(file_path, 'r', encoding=encoding, errors='replace') as file:
+def read_chunk(file_path):
+    with open(file_path, 'r') as file:
         return file.read()
 
 def map_function(chunk):
-    word_count = []
     words = re.findall(r'\b\w+\b', chunk)
-    
-    for word in words:
-        word_count.append((word, 1))
-    
+    word_count = [(word, 1) for word in words]
     return word_count
 
 def shuffle_and_sort(mapped_results):
     sorted_results = defaultdict(list)
     
-    for item in mapped_results:
-        word, count = item
+    for word, count in mapped_results:
         sorted_results[word].append(count)
     
     return sorted_results.items()
 
+def reduce_function(sorted_results):
+    reduced_results = [(word, sum(counts)) for word, counts in sorted_results]
+    return reduced_results
 
 def save_to_file(result, file_path, output_dir='logs'):
     log_filename = os.path.join(output_dir, f'{file_path}')
@@ -39,44 +37,6 @@ def read_result_from_file(file_path):
         # Convert lists back to tuples
         return [tuple(item) for item in loaded_result]
     
-def map_task(file_path):
-    chunk = read_chunk(file_path)
-    mapped_result = map_function(chunk)
-    filename = os.path.basename(file_path)  
-    save_to_file(mapped_result,filename.replace('.txt', '') + '_map.txt','mapStep')  #Save map result to chunk_x_map.txt
-    return filename.replace('.txt', '') + '_map.txt'
-    
-def group_task(dir_path):
-    
-    temp_group = []
-    
-    for filename in os.listdir(dir_path):    
-        file_path = os.path.join(dir_path, filename)
-        loaded_map = read_result_from_file(file_path)
-        temp_group.extend(loaded_map)
-        
-    sorted_results = shuffle_and_sort(temp_group)
-    save_to_file(sorted_results, filename.replace('.txt', '') + '_group','groupStep')   
-  
-  
-def reduce_function(sorted_results):
-    reduced_results = []
-    
-    for word, counts in sorted_results:
-        total_count = sum(counts)
-        reduced_results.append((word, total_count))
-    
-    return reduced_results
-
-        
-def reduce_task(dir_path):
-    for filename in os.listdir(dir_path):    
-        file_path = os.path.join(dir_path, filename)
-        loaded_group = read_result_from_file(file_path)
-        reduced_results = reduce_function(loaded_group)
-        save_to_file(reduced_results, filename.replace('.txt', '') + '_reduce','reduceStep')
-    
-
 def map_reduce(input_dir="chunks"):
     os.makedirs("mapStep", exist_ok=True)
     os.makedirs("groupStep", exist_ok=True)
