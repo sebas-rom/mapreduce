95120e2b5dfe9b09744c30ecb26fa49688d73143
Juanra300o2 <157748083+Juanra300o2@users.noreply.github.com>
2024-01-28 19:11:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 import threading
 import concurrent.futures
 from file_splitter import split_and_lowercase
-from map_reduce import read_chunk,map_function,save_to_file
+from map_reduce import read_chunk,map_function,save_to_file,read_result_from_file,shuffle_and_sort,reduce_function
 exitFlag = 0
 # Global variable to signal the threads to stop
 stop_threads = False
 
 
 class mapNode(threading.Thread):
-    def __init__(self, threadID, name, counter, input_file):
+    def __init__(self, threadID, name, counter, input_file): 
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
@@ -27,6 +27,48 @@ def map_f(input_file):
     chunk = read_chunk(file_path)
     mapped_result = map_function(chunk)
     save_to_file(mapped_result, input_file.replace('.txt', '') + '_map', 'mapStep')
+
+class groupNode(threading.Thread):
+    def __init__(self, threadID, name, counter, input_file): 
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.input_file = input_file
+
+    def run(self):
+        print("Starting " + self.name)
+        group_f(self.input_file)
+        print("Exiting " + self.name)
+
+def group_f(input_file):
+        file_path = os.path.join('mapStep', input_file)
+        loaded_map = read_result_from_file(file_path)
+        sorted_results = shuffle_and_sort(loaded_map)
+        save_to_file(sorted_results, input_file.replace('.txt', '') + '_group','groupStep')
+
+class reduceNode(threading.Thread):
+    def __init__(self, threadID, name, counter, file_pair): 
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.file_pair = file_pair
+
+    def run(self):
+        print("Starting " + self.name)
+        reduce_f(self.file_pair[0], self.file_pair[1])
+        print("Exiting " + self.name)
+
+def reduce_f(input_file1, input_file2):
+        file_path1 = os.path.join("groupStep", input_file1)
+        file_path2 = os.path.join("groupStep", input_file2)
+
+        loaded_group = read_result_from_file(file_path1)+read_result_from_file(file_path2)
+        reduced_results = reduce_function(loaded_group)
+        save_to_file(reduced_results, input_file1.replace('.txt', '') + '_to_' +input_file2.replace('.txt', '')+ '_reduce','reduceStep')
+
+
 if __name__ == "__main__":
     os.makedirs("mapStep", exist_ok=True)
     os.makedirs("groupStep", exist_ok=True)
@@ -46,8 +88,22 @@ if __name__ == "__main__":
                 future = executor.submit(node.run)
                 threads.append(future)
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        for filename in os.listdir("mapStep"):           
+            node = groupNode(1, f"groupNode-{filename}", 4, filename)
+            future = executor.submit(node.run)
+            threads.append(future)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        filenames = sorted(os.listdir("groupStep"))
+        pairs = [(filenames[i], filenames[i + 1]) for i in range(0, len(filenames), 2)]
+        for file_pair in pairs:           
+            node = reduceNode(1, f"reduceNode-{file_pair[0]}-{file_pair[1]}", 2, file_pair)
+            future = executor.submit(node.run)
+            threads.append(future)
+    
+
+
     # Wait for all threads to complete
     concurrent.futures.wait(threads)
 
     print("Exiting Map Thread")
-
