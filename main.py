ca9ac0df5fe96b0d08c4c8332e96119197821630
Kristian Mendoza <saidmendoza730@gmail.com>
2024-01-30 12:41:08 -0500

    last test

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def runComputers(computer_number):
     print("Exiting Main Thread")
 if __name__ == "__main__":
 
-    input_file_path = "texts/test.txt"
+    input_file_path = "texts/bible.txt"
 
     split_and_lowercase(input_file_path)
 
