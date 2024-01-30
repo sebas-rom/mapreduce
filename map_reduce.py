50940bab8872303a1d812314cb82874febe8eb9e
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-01-30 10:40:49 -0500

    improved map reduce

--- map_reduce.py
+++ map_reduce.py
@@ -9,8 +9,10 @@ def read_chunk(file_path):
 
 def map_function(chunk):
     words = re.findall(r'\b\w+\b', chunk)
-    word_count = [(word, 1) for word in words]
-    return word_count
+    word_count = defaultdict(int)
+    for word in words:
+        word_count[word] += 1
+    return word_count.items()
 
 def shuffle_and_sort(mapped_results):
     sorted_results = defaultdict(list)
@@ -36,38 +38,3 @@ def read_result_from_file(file_path):
         loaded_result = json.load(file)
         # Convert lists back to tuples
         return [tuple(item) for item in loaded_result]
-    
-def map_reduce(input_dir="chunks"):
-    os.makedirs("mapStep", exist_ok=True)
-    os.makedirs("groupStep", exist_ok=True)
-    os.makedirs("reduceStep", exist_ok=True)
-    
-    # Map step
-    for filename in os.listdir(input_dir):
-        if filename.endswith(".txt"):
-            file_path = os.path.join(input_dir, filename)
-            chunk = read_chunk(file_path)
-            mapped_result = map_function(chunk)  
-            save_to_file(mapped_result,filename.replace('.txt', '') + '_map','mapStep')  #Save map result to chunk_x_map.txt
-
-    # #group step
-    temp_group = []
-    for filename in os.listdir('mapStep'):    
-        file_path = os.path.join('mapStep', filename)
-        loaded_map = read_result_from_file(file_path)
-        temp_group.extend(loaded_map)
-
-    sorted_results = shuffle_and_sort(temp_group)
-    save_to_file(sorted_results, filename.replace('.txt', '') + '_group','groupStep')
-
-    #Reduce
-    for filename in os.listdir('groupStep'):    
-        file_path = os.path.join('groupStep', filename)
-        loaded_group = read_result_from_file(file_path)
-        reduced_results = reduce_function(loaded_group)
-        save_to_file(reduced_results, filename.replace('.txt', '') + '_reduce','reduceStep')
-        
-
-if __name__ == "__main__":
-    
-    map_reduce()
